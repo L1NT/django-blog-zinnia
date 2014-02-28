859110f14c9661138a98dd2c9ee2a4d725c9ff26
Luke Jeter <luke.jeter@gmail.com>
2014-02-27 19:52:47 -0600

    Add DJANGO_VERSION to version context processor.

--- zinnia/context_processors.py
+++ zinnia/context_processors.py
@@ -1,9 +1,13 @@
 """Context Processors for Zinnia"""
 from zinnia import __version__
+import django
 
 
 def version(request):
     """
-    Add version of Zinnia to the context.
+    Add version of Zinnia and Django to the context.
     """
-    return {'ZINNIA_VERSION': __version__}
+    return {
+        'ZINNIA_VERSION': __version__,
+        'DJANGO_VERSION': django.get_version(),
+    }
